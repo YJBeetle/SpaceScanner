ee01359382c2b46afd3bc70e5902bd6ec2cb334d
YJBeetle <YJBeetle@YJBeetle-MacPro.local>
2018-10-09 03:32:49 +0800

    大致确定了下NavigationDrawer的内容

--- websrc/src/App.js
+++ websrc/src/App.js
@@ -1,24 +1,67 @@
 import React, { Component } from 'react';
-import { NavigationDrawer, Button, FontIcon } from 'react-md';
-import logo from './logo.svg';
+import { NavigationDrawer, Button, FontIcon, Card, CardTitle, CardText, CardActions } from 'react-md';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <NavigationDrawer
-        drawerTitle="react-md with CRA"
-        toolbarTitle="Welcome to react-md"
+        drawerTitle={<div onClick={() => { console.log("click title") }}>Space Scanner</div>}
+        toolbarTitle="Welcome"
+        navItems={
+          [
+            {
+              primaryText: 'Space scan',
+              subheader: true,
+            },
+            {
+              key: 'start',
+              primaryText: 'Start new scan',
+              leftIcon: <FontIcon>send</FontIcon>,
+              onClick: () => { console.log("click start") },
+              active: true,
+            },
+            {
+              key: '1',
+              primaryText: '/opt',
+              leftIcon: <FontIcon>insert_chart</FontIcon>,
+              onClick: () => { console.log("click 1") },
+            },
+            {
+              key: '2',
+              primaryText: '/',
+              leftIcon: <FontIcon>insert_chart</FontIcon>,
+              onClick: () => { console.log("click 2") },
+            },
+            {
+              key: '3',
+              primaryText: '/usr',
+              leftIcon: <FontIcon>insert_chart</FontIcon>,
+              onClick: () => { console.log("click 3") },
+            },
+            {
+              divider: true,
+            },
+          ]
+        }
+      // toolbarActions={<Button icon onClick={}>close</Button>}
       >
         <div className="App">
-          <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h2>Welcome to React</h2>
-          </div>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
           <Button raised primary iconEl={<FontIcon>home</FontIcon>}>Button</Button>
+
+          <div className="md-grid">
+            <Card className="md-cell">
+              <CardTitle title="Hello, World!" />
+              <CardText>
+                Lorem ipsum... pretend more ...
+          </CardText>
+              <CardActions>
+                <Button flat label="Action 1" />
+                <Button flat label="Action 2" />
+              </CardActions>
+            </Card>
+          </div>
+
         </div>
       </NavigationDrawer>
     );
