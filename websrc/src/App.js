2eb7fc7dcea61e837cecb31854e05fb0a2577f02
YJBeetle <YJBeetle@YJBeetle-MacPro.lan>
2018-10-09 01:05:59 +0800

    reinit websrc to react-md

--- websrc/src/App.js
+++ websrc/src/App.js
@@ -1,26 +1,26 @@
 import React, { Component } from 'react';
+import { NavigationDrawer, Button, FontIcon } from 'react-md';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
+      <NavigationDrawer
+        drawerTitle="react-md with CRA"
+        toolbarTitle="Welcome to react-md"
+      >
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to React</h2>
+          </div>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+          <Button raised primary iconEl={<FontIcon>home</FontIcon>}>Button</Button>
+        </div>
+      </NavigationDrawer>
     );
   }
 }
