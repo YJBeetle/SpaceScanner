bc416d065d6d698b9517834cba76d2ec64350f41
YJBeetle <YJBeetle@YJBeetle-MacPro.local>
2018-10-09 22:22:46 +0800

    home 界面ok

--- websrc/src/Home.js
+++ websrc/src/Home.js
@@ -1,26 +1,25 @@
 import React, { Component } from 'react';
-import { NavigationDrawer, Button, FontIcon, Card, CardTitle, CardText, CardActions } from 'react-md';
+import { Link } from 'react-router';
+import { Button, FontIcon, Card, CardTitle, CardText, CardActions } from 'react-md';
 import './home.css';
 
 export default class Home extends Component {
   render() {
     return (
-        <div className="home">
-          <Button raised primary iconEl={<FontIcon>home</FontIcon>}>Button</Button>
-
-          <div className="md-grid">
-            <Card className="md-cell">
-              <CardTitle title="Hello, World!" />
-              <CardText>
-                Lorem ipsum... pretend more ...
+      <div className="home md-grid">
+        <h2 className="md-cell md-cell--12 welcome">
+          Welcome to Space Scanner
+        </h2>
+        <Card className="md-cell md-cell--6">
+          <CardTitle title="Disk usage" />
+          <CardText>
+            <p>scan your disk</p>
+            <p>
+              <Button raised primary iconEl={<FontIcon>send</FontIcon>} component={Link} to='/dustart'>Start</Button>
+            </p>
           </CardText>
-              <CardActions>
-                <Button flat label="Action 1" />
-                <Button flat label="Action 2" />
-              </CardActions>
-            </Card>
-          </div>
-        </div>
+        </Card>
+      </div>
     );
   }
 }
