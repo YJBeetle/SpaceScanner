e8b2693609bd41fa4face60a6f5af4de69185ed1
YJBeetle <yjbeetle@gmail.com>
2018-10-11 04:17:09 +0800

    按钮改为图标+tooltip

--- websrc/src/Du.js
+++ websrc/src/Du.js
@@ -9,15 +9,15 @@ export default class Page2 extends Component {
             <div className="du">
                 <Card className="tools">
                     <div>
-                        <Button className="button" flat secondary iconChildren="find_replace">Refresh</Button>
-                        <Button className="button" flat secondary iconChildren="view_stream">Less detail</Button>
-                        <Button className="button" flat secondary iconChildren="view_quilt">More detail</Button>
-                        <Button className="button" flat secondary iconChildren="info">Info</Button>
+                        <Button className="button" icon secondary iconChildren="find_replace" tooltipLabel="Refresh" tooltipPosition="right">Refresh</Button>
+                        <Button className="button" icon secondary iconChildren="view_stream" tooltipLabel="Less detail" tooltipPosition="right">Less detail</Button>
+                        <Button className="button" icon secondary iconChildren="view_quilt" tooltipLabel="More detail" tooltipPosition="right">More detail</Button>
+                        <Button className="button" icon secondary iconChildren="info" tooltipLabel="Info" tooltipPosition="right">Info</Button>
                     </div>
                     <Divider />
                     <div className="dir">
-                    <Chip className="folder" label="Root directory" avatar={<Avatar><FontIcon>home</FontIcon></Avatar>} /><FontIcon>navigate_next</FontIcon>
-                    <Chip className="folder" label="A_Folder" avatar={<Avatar random>A</Avatar>} /><FontIcon>navigate_next</FontIcon>
+                        <Chip className="folder" label="Root directory" avatar={<Avatar><FontIcon>home</FontIcon></Avatar>} /><FontIcon>navigate_next</FontIcon>
+                        <Chip className="folder" label="A_Folder" avatar={<Avatar random>A</Avatar>} /><FontIcon>navigate_next</FontIcon>
                         <Chip className="folder" label="B_Folder" avatar={<Avatar random>B</Avatar>} /><FontIcon>navigate_next</FontIcon>
                         <Chip className="folder" label="C_Folder" avatar={<Avatar random>C</Avatar>} />
                     </div>
